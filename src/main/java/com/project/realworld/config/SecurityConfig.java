9cd32d1e1c0288743d2cc257e1567c5a1299763b
Jukyung <sungjukyung94@gmail.com>
2024-02-19 23:49:06 +0900

    add csrf in Security config

--- src/main/java/com/project/realworld/config/SecurityConfig.java
+++ src/main/java/com/project/realworld/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,7 +16,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/**").permitAll());
+                        .requestMatchers("/api/**").permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
     }
 }
